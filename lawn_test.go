f62675f94dcf5df3624019003f7b572e8a8f3f0f
Julien Dehee <julien.dehee@gmail.com>
2019-03-09 20:24:25 +0100

    support multiple mow() test cases

--- lawn_test.go
+++ lawn_test.go
@@ -73,21 +73,61 @@ FFRFFRFRRF
 	}
 }
 
+var mowTestCases = []struct {
+	lawnWidth, lawnHeight                      int
+	mowerXPos, mowerYPos                       int
+	mowerOrientation                           string
+	mowerInstructions                          []string
+	expectedMowerEndXPos, expectedMowerEndYPos int
+	expectedMowerEndOrientation                string
+}{
+	{
+		lawnWidth:                   5,
+		lawnHeight:                  6,
+		mowerXPos:                   1,
+		mowerYPos:                   2,
+		mowerOrientation:            "N",
+		mowerInstructions:           []string{"F", "F", "F", "F", "F", "F", "F", "F"},
+		expectedMowerEndXPos:        1,
+		expectedMowerEndYPos:        6,
+		expectedMowerEndOrientation: "N",
+	},
+	{
+		lawnWidth:                   3,
+		lawnHeight:                  5,
+		mowerXPos:                   1,
+		mowerYPos:                   1,
+		mowerOrientation:            "E",
+		mowerInstructions:           []string{"L", "F", "F", "R", "F", "F", "F", "F", "R"},
+		expectedMowerEndXPos:        3,
+		expectedMowerEndYPos:        3,
+		expectedMowerEndOrientation: "S",
+	},
+}
+
 func TestMow(t *testing.T) {
-	lawn := lawn{
-		width:  5,
-		height: 6,
-		mowers: []*mower{&mower{
-			orientation:  "N",
-			xPos:         1,
-			yPos:         2,
-			instructions: []string{"F", "F", "F", "F", "F", "F", "F", "F"},
-		},
-		},
-	}
-	lawn.mow()
-	mower := lawn.mowers[0]
-	if mower.xPos != 1 || mower.yPos != 6 || mower.orientation != "N" {
-		t.Fatalf("Wrong final position/orientation, expected 1 3 N, got %v %v %s", mower.xPos, mower.yPos, mower.orientation)
+	for _, tc := range mowTestCases {
+		lawn := lawn{
+			width:  tc.lawnWidth,
+			height: tc.lawnHeight,
+			mowers: []*mower{&mower{
+				orientation:  tc.mowerOrientation,
+				xPos:         tc.mowerXPos,
+				yPos:         tc.mowerYPos,
+				instructions: tc.mowerInstructions,
+			},
+			},
+		}
+		lawn.mow()
+		mower := lawn.mowers[0]
+		if mower.xPos != tc.expectedMowerEndXPos ||
+			mower.yPos != tc.expectedMowerEndYPos ||
+			mower.orientation != tc.expectedMowerEndOrientation {
+			t.Fatalf("Wrong final position/orientation, expected %v %v %s, got %v %v %s",
+				tc.expectedMowerEndXPos,
+				tc.expectedMowerEndYPos,
+				tc.expectedMowerEndOrientation,
+				mower.xPos, mower.yPos, mower.orientation)
+		}
 	}
 }
